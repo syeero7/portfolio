3234cf2d19763e615de4bb07b8a52c384dac5505
Emal <EmalHasitha@proton.me>
2025-08-03 13:10:41 +0530

    fix: remove dropdown menu

--- src/lib/nav-menu.ts
+++ src/lib/nav-menu.ts
@@ -1,4 +1,3 @@
-const dropdown = document.querySelector("#dropdown") as HTMLInputElement;
 const nav = document.querySelector("nav")!;
 
 export function initializeNavbarHandler() {
@@ -8,5 +7,4 @@ export function initializeNavbarHandler() {
 function handleDropdownClick(e: MouseEvent) {
   if (!e.target || (e.target as HTMLElement).tagName !== "A") return;
   if ((e.target as HTMLElement).dataset.logo) window.scrollTo({ top: 0 });
-  dropdown.checked = false;
 }
